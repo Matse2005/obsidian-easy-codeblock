0c964292a23cacc25cb4f40bf002f49e0753752d
MatseVH <49431416+Matse2005@users.noreply.github.com>
2025-12-13 08:42:20 +0100

    Made the plugin review bot compliant

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 import { Plugin } from 'obsidian';
 import { CommandRegistry } from 'src/Commands/CommandRegistry';
 
-export default class MyPlugin extends Plugin {
+export default class EasyCodeBlock extends Plugin {
 
-	async onload() {
+	onload() {
 		const registry = new CommandRegistry(this);
 		registry.registerAll();
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
